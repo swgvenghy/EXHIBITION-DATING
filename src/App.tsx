5f32ca32af05c72c0b7dff7a9869d77e66e67074
Choi JunHo <duekzim@gmail.com>
2025-02-18 00:29:45 +0900

    Chore: 프로젝트 세팅

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className=' bg-black h-dvh w-dvw text-green-500 text-2xl flex justify-center items-center flex-col'>
+        COW-DATE
+      </div>
+    </>
+  );
+}
+
+export default App;
