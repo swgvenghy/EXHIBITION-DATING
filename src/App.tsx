7bd0598a4f436fe8c2662494ee456f87cfcfb377
swgvenghy <duekzim@gmail.com>
2025-02-18 17:12:56 +0900

    feat: landing page 제작

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,52 @@
+import { useNavigate } from "react-router-dom";
+import CowSvg from "./assets/cow-logo.svg?react";
+
 function App() {
+  const navigate = useNavigate();
   return (
-    <>
-      <div className=' bg-black h-dvh w-dvw text-green-500 text-2xl flex justify-center items-center flex-col'>
-        COW-DATE
+    <div className="px-4 py-6 w-dvw h-dvh flex flex-col gap-1 font-sans">
+      <CowSvg className="fill-black" />
+      <h1 className=" font-semibold text-3xl">COW 5기 모집중 🔍</h1>
+      <div className=" text-lg font-semibold">
+        우리와 함께 더 나은 명지를 <br />
+        만들어갈 인재를 기다립니다.
       </div>
-    </>
+      <div className=" font-semibold">2025.03.04 ~ 2025.03.12</div>
+      <section className=" pt-5 flex flex-col items-center gap-6">
+        <button
+          onClick={() => {
+            navigate("/signup");
+          }}
+          className=" w-64 flex items-center justify-center py-5 font-bold text-2xl shadow-xl rounded-2xl"
+        >
+          소개팅 카드 만들기
+        </button>
+        <button
+          onClick={() => {
+            navigate("/dashboard");
+          }}
+          className=" w-64 flex items-center justify-center py-5 font-bold text-2xl shadow-xl rounded-2xl"
+        >
+          다른 사람 카드 보러가기
+        </button>
+        <button
+          onClick={() => {
+            window.open("https://ddingdong.mju.ac.kr/club/91");
+          }}
+          className=" w-64 flex items-center justify-center py-5 font-bold text-2xl shadow-xl rounded-2xl"
+        >
+          COW 지원하기
+        </button>
+        <button
+          onClick={() => {
+            window.open("https://www.instagram.com/mju_cow/");
+          }}
+          className="w-64 flex items-center justify-center py-5 font-bold text-2xl shadow-xl rounded-2xl"
+        >
+          COW 인스타그램
+        </button>
+      </section>
+    </div>
   );
 }
 
